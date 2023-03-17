5f61b7ae27ed0cae376cb9e0180ed51faf4743d8
fredyang <fredyang@semanticsworks.com>
2023-03-17 00:10:37 -0400

    2 files modified, 2 added

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  host: {
+    'class': 'box'
+  }
 })
 export class ProfileComponent implements OnInit {
 
